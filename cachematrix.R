67b9f3dd094ecd66e585fcc05414c7b29ce0e19b
Sylvia Weiher <sylvia.weiher@googlemail.com>
2015-07-21 15:41:56 +0200

    Insert solution

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  ## list contains the environments, where the object is stored
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -12,4 +24,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ## assign a potential cached result to m
+  m <- x$getsolve()
+  
+  ## If matrix has allready been stored, get results out of cache.
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ## otherwise, do the calculation and 
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  ## and cache result with setsolve
+  x$setsolve(m)
+  m
 }
